bc4a5b0ba4d2d1bad925b12cfb32ea40cf376f39
SnehaOsta <snehaosta4@gmail.com>
2025-10-09 13:10:13 +0530

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <div><p>chapter01-v4.1 buwgugelu</p></div>
+      <div><p>chapter01-v4.1</p></div>
       <div>
         <Image src="/Logo.png"  alt="Company Logo"  width={150}  height={50} />
       </div>
